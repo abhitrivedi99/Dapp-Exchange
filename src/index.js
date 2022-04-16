4648a22635e7c8d3806b46672c81435893acfdd4
abhitrivedi99 <abhitrivedi99@gmail.com>
2022-04-16 22:05:07 +0530

    css apply change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
